80266c826330fec4f308b44b8672d61413c79ff0
Chethiya Galkaduwa <84511412+chey97@users.noreply.github.com>
2024-02-03 11:22:10 +0530

    hot commit

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function Greet(){
+//     return <h1>Hello Chethiya</h1>
+// }
+
+const Greet = () => <h1>Hello Chethiya!</h1>; //arrow function
+
+export default Greet;
